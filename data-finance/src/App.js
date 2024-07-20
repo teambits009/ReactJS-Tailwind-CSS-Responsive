e1caf1cbf69d20f59cc5d71c3431232ec5c23405
Brandon <teambitsandy@gmail.com>
2024-07-20 22:05:14 +0300

    components

---
+++ data-finance/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
